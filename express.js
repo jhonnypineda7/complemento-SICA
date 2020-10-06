a429ae1756f9cb4b839a3e5697d94736df0a1a82
Daniel Andres Valencia Ruiz (SENA) <daniel.valencia@Comsistelco.local>
2020-10-06 11:28:38 -0500

    .

--- express.js
+++ express.js
@@ -2,9 +2,6 @@ var express = require('express');
 var excels = require('./validacion.js');
 var bodyParser = require('body-parser');
 var multer = require('multer');
-const fs = require('fs')
-var MongoClient = require('mongodb').MongoClient;
-var url = "mongodb+srv://jhonny95:369852147@1-1.xxloa.mongodb.net/Archivosdb?retryWrites=true&w=majority";
 
 storage = multer.diskStorage({
     destination: function (req, file, cb) {
@@ -22,60 +19,36 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }))
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/seleccionModulo.html');
+    res.sendFile(__dirname + '/seleccioneModulo.html');
 })
 
 app.post('/validacion_de_archivos', upload.single('archivo'), (req, res) => {
-       MongoClient.connect(url, function (err, db) {
-        if (err) throw err;
-        var dbo = db.db("Archivosdb");
-        var myobj = { modulo: `${modulo}`, name: `${fileName}`, totalEncabezados: `${excels.celdasTotales}` };
-        dbo.collection("archivos").insertOne(myobj, function (err, res) {
-            if (err) throw err;
-            console.log("1 document inserted");
-            db.close();
-        });
-    }) 
+
+    // if (req.file.originalname === undefined ) {
+    //     next(new Error('Not valid name'));
+    // }
     fileName = req.file.originalname
     modulo = req.body.modulo
-    excels.validar(modulo, fileName, `archivos/${fileName}`);
-    res.send(`<h1 style="text-align:center;margin-top:300px;font-family: Arial, Helvetica, sans-serif;"> 
-                El archivo:${fileName}<br>${excels.mensajeEvaluacion} 
-                <a href="https://calm-basin-93401.herokuapp.com/"><br>
-                <button style="font-size:25px; border-radius: 10px;">INICIO</button></a>
-              </h1>`
+    excels.validar(modulo, `${fileName}`, `archivos/${fileName}`);
+    
+    let respuesta = `<h1 style="text-align:center;margin-top:300px;font-family: Arial, Helvetica, sans-serif;"> 
+                        <center>${modulo}</center>
+                        El archivo: ${fileName}<br>${excels.mensajeEvaluacion}
+                        <a href="https://pruebadevalidacion.herokuapp.com/"><br><br>
+                        <button style="font-size:25px; border-radius: 10px;">INICIO</button></a>
+                    </h1>`
 
-    )
+    res.send(respuesta)
 
-    MongoClient.connect(url, function (err, db) {
-        if (err) throw err;
-        var dbo = db.db("Archivosdb");
-        var myobj = { modulo: `${modulo}`, name: `${fileName}`, totalEncabezados: `${excels.celdasTotales}` };
-        dbo.collection("archivos").insertOne(myobj, function (err, res) {
-            if (err) throw err;
-            console.log("1 documento insertado");
-            db.close();
-        });
-    })
-    fs.unlink('./archivos/' + fileName, (err) => {
-        if (err) {
-            console.error(err)
-            return
-        }
-    })
 });
 
 app.use(function (err, req, res, next) {
-    console.log(err.stack);
-    fs.unlink('./archivos/' + fileName, (err) => {
-        if (err) {
-            console.error(err)
-            return
-        }
-    })
-    return res.status(500).send(`<h1 style="text-align:center;margin-top:300px;font-family: Arial, Helvetica, sans-serif;">El archivo no coincide con el modulo seleccionado<a href="https://calm-basin-93401.herokuapp.com/"><br><br>
-    <button style="font-size:25px; border-radius: 10px;">INICIO</button></a></h1>`);
+
+    let error = `<h1 style="text-align:center;margin-top:300px;font-family: Arial, Helvetica, sans-serif;">El archivo no coincide con el modulo seleccionado<a href="https://pruebadevalidacion.herokuapp.com/">
+                <br><br><button style="font-size:25px; border-radius: 5px;">INICIO</button></a></h1>`
+
+    return res.status(500).send(error);
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT);
--- validacion.js
+++ validacion.js
@@ -1,8 +1,6 @@
 const excelToJson = require('convert-excel-to-json')
-const fs = require('fs')
 const headers = require('./encabezados')
-let nombreModulo, nombreActividad, archivo;
-
+// 
 exports.validar = (nombreModulo, nombreActividad, archivo) => {
 
     let validacionEncabezados;
@@ -16,40 +14,39 @@ exports.validar = (nombreModulo, nombreActividad, archivo) => {
         let celdas = archivoActual[element]
         encabezadosTodos.push(Object.values(celdas[0]))
     };
-    console.log(encabezadosTodos.length + ":" + encabezadosPredeterminados.length);
-
+    
     for (let i = 0; i < encabezadosTodos.length && encabezadosPredeterminados.length; i++) {
 
         let celdasActuales = encabezadosTodos[i], celdasPredeterminadas = encabezadosPredeterminados[i];
+        
         if (celdasActuales.length === celdasPredeterminadas.length) {
-            exports.celdasTotales = celdasActuales.length
+
             for (let j = 0; j < celdasActuales.length; j++) {
-                // console.log(celdasActuales[j], "------", celdasPredeterminadas[j])
+
                 for (let k = 0; k < celdasPredeterminadas.length; k++) {
+
                     if (celdasActuales[j] !== celdasPredeterminadas[k]) {
                         validacionEncabezados = false
                     } else {
                         validacionEncabezados = true
-                        console.log(celdasActuales[j], "-----", celdasPredeterminadas[k]);
                         break
                     }
                 }
+
                 if (validacionEncabezados === false) {
                     break
                 }
             }
         } else {
-            console.log("lA CANTIDAD DE CELDAS NO ES CORRECTA")
             validacionEncabezados = false
             break
         }
     };
+
     if (validacionEncabezados === true) {
-        console.log("validado")
-        exports.mensajeEvaluacion = `es valido`
+        console.log(archivo);
+        exports.mensajeEvaluacion = `--Es valido--`
     } else {
-        console.log("no es valido")
-        exports.mensajeEvaluacion = `no es valido:
-        las celdas no coinciden`
+        exports.mensajeEvaluacion = `No es valido: las celdas no coinciden`
     };
 };
